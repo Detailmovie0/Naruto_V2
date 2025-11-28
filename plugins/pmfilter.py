d6e751a66865446526193c54b4a1f9c49c2a6850
Detailmovie0 <moviehub4uofficial0@gmail.com>
2025-11-28 08:56:32 +0530

    Refactor inline keyboard buttons in pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1519,10 +1519,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
           #  InlineKeyboardButton(' ᴅɪꜱᴄʟᴀɪᴍᴇʀ ', callback_data='disclaimer'),
             InlineKeyboardButton ('sᴏᴜʀᴄᴇ', callback_data='source'),
+      #  ],[
+         #   InlineKeyboardButton('ᴅᴏɴᴀᴛɪᴏɴ ', callback_data='donation'),
         ],[
-            InlineKeyboardButton('ᴅᴏɴᴀᴛɪᴏɴ ', callback_data='donation'),
-     #   ],[
-          #  InlineKeyboardButton('⇋ ʙᴀᴄᴋ ᴛᴏ ʜᴏᴍᴇ ⇋', callback_data='start')
+            InlineKeyboardButton('⇋ ʙᴀᴄᴋ ᴛᴏ ʜᴏᴍᴇ ⇋', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
