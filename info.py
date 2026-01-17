c0d1f213a2935e7ce1c7ac17633b4a7872980f83
Detailmovie0 <moviehub4uofficial0@gmail.com>
2026-01-17 08:39:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002635477308')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/disscus_moviehub4u')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003530497206, -1003081942933")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003530497206 -1003081942933")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
 auth_channels     = environ.get("AUTH_CHANNELS", "-1002282783745 -1002393557941 -1002594005991 -1002497307753")# Channels for force sub (make sure bot is admin)
 
 # ============================
